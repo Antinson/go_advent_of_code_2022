f5b9be5cee6d6c427ff147331494716055a8a5e4
Antinson <ant.mitchinson@gmail.com>
2025-09-11 15:36:19 +1200

    day3 part 1 done

--- day3/p1.go
+++ day3/p1.go
@@ -6,28 +6,58 @@ import (
 	"os"
 )
 
-func splitAndGroup(a string) {
+func mapLettersa(letter int) int {
+	if letter >= 97 {
+		return letter - 96
+	} else {
+		return letter - 64 + 26
+	}
+}
 
-	firstSack := a[:len(a)/2]
-	//secondSack := a[:len(a)/2]
+func splitAndGroup(a string) int {
 
-	for letter := range firstSack {
-		fmt.Println(letter)
+	firstSack := a[0 : len(a)/2]
+	secondSack := a[len(a)/2:]
+
+	checkValues := make(map[int]int, 5)
+
+	for i, value := range firstSack {
+		for j := range secondSack {
+			if firstSack[i] == secondSack[j] {
+				checkValues[int(value)] = mapLettersa(int(value))
+			}
+		}
 	}
 
-	//fmt.Printf("%v \t\t %v\n", firstSack, secondSack)
+	total := 0
+
+	for _, value := range checkValues {
+		total += int(value)
+	}
+
+	return total
+
 }
 
 func p1() {
 
-	data, _ := os.Open("test.txt")
+	data, err := os.Open("input.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
 	defer data.Close()
 
 	scanner := bufio.NewScanner(data)
 
+	totalScore := 0
+
 	for scanner.Scan() {
 		line := scanner.Text()
-		splitAndGroup(line)
+		//fmt.Println(line)
+		totalScore += splitAndGroup(line)
 	}
+
+	fmt.Println(totalScore)
 
 }
